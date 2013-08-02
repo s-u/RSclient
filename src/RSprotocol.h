35febbfb71c8d4bf626b05748a4a673e9728447a
Simon Urbanek <simon.urbanek@R-project.org>
2013-08-02 15:23:43 +0000

    update RSprotocol.h for OC

--- src/RSprotocol.h
+++ src/RSprotocol.h
@@ -133,6 +133,9 @@ struct phdr { /* always 16 bytes */
 #define CMD_PROXY_TARGET    (CMD_CLASS_PROXY | 0x01) /* payload is NUL-terminted string defining the desired target host:port -- IPv6 addresses must be quoted in [] */
 #define CMD_PROXY_GET_SLOT  (CMD_CLASS_PROXY | 0x02) /* no payload, lock-in a slot for connection-limited hosts, proxy closes connection if the target slot is unavailable at this point */
 
+/* flag for create_server: Use QAP object-cap mode */
+#define SRV_QAP_OC 0x40
+
 /* stat codes; 0-0x3f are reserved for program specific codes - e.g. for R
    connection they correspond to the stat of Parse command.
    the following codes are returned by the Rserv itself
@@ -207,6 +210,15 @@ struct phdr { /* always 16 bytes */
 #define CMD_switch       0x005 /* string (protocol)  : - */
 #define CMD_keyReq       0x006 /* string (request) : bytestream (key) */ 
 #define CMD_secLogin     0x007 /* bytestream (encrypted auth) : - */
+
+#define CMD_OCcall       0x00f /* SEXP : SEXP  -- it is the only command
+								  supported in object-capability mode
+								  and it requires that the SEXP is a
+								  language construct with OC reference
+								  in the first position */
+#define CMD_OCinit  0x434f7352 /* SEXP -- 'RsOC' - command sent from
+								  the server in OC mode with the packet
+								  of initial capabilities. */
 
 /* file I/O routines. server may answe */
 #define CMD_openFile     0x010 /* fn : - */
