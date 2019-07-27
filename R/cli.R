6cfe46c7383eaf4a4c7c3b0876dff3c97c46eb92
Simon Urbanek <simon.urbanek@R-project.org>
2019-07-26 20:30:18 -0400

    use NAMESPACE for symbol registration

--- R/cli.R
+++ R/cli.R
@@ -1,16 +1,16 @@
-RS.connect <- function(host=NULL, port=6311L, tls=FALSE, verify=TRUE, proxy.target=NULL, proxy.wait=TRUE) .Call("RS_connect", host, port, tls, proxy.target, proxy.wait, verify, PACKAGE="RSclient")
+RS.connect <- function(host=NULL, port=6311L, tls=FALSE, verify=TRUE, proxy.target=NULL, proxy.wait=TRUE) .Call(RS_connect, host, port, tls, proxy.target, proxy.wait, verify)
 
-RS.close <- function(rsc) .Call("RS_close", rsc)
+RS.close <- function(rsc) .Call(RS_close, rsc)
 
-RS.eval <- function(rsc, x, wait=TRUE, lazy=TRUE) { r <- .Call("RS_eval", rsc, serialize(if (isTRUE(lazy)) substitute(x) else x, NULL, FALSE), wait, PACKAGE="RSclient"); if (is.raw(r)) unserialize(r) else r }
+RS.eval <- function(rsc, x, wait=TRUE, lazy=TRUE) { r <- .Call(RS_eval, rsc, serialize(if (isTRUE(lazy)) substitute(x) else x, NULL, FALSE), wait); if (is.raw(r)) unserialize(r) else r }
 
-RS.eval.qap <- function(rsc, x, wait=TRUE) .Call("RS_eval_qap", rsc, x, wait, PACKAGE="RSclient")
+RS.eval.qap <- function(rsc, x, wait=TRUE) .Call(RS_eval_qap, rsc, x, wait)
 
 RS.collect <- function(rsc, timeout = Inf, detail = FALSE, qap = FALSE) {
-    r <- .Call("RS_collect", rsc, timeout, PACKAGE="RSclient")
+    r <- .Call(RS_collect, rsc, timeout)
     if (is.raw(r)) {
         if (length(r)) {
-            val <- if (qap) .Call("RS_decode", r) else unserialize(r)
+            val <- if (qap) .Call(RS_decode, r) else unserialize(r)
             if (isTRUE(detail))
                 list(value = val, rsc = attr(r, "rsc"))
             else val
@@ -20,15 +20,15 @@ RS.collect <- function(rsc, timeout = Inf, detail = FALSE, qap = FALSE) {
     } else r
 }
 
-RS.server.eval <- function(rsc, text) .Call("RS_ctrl_str", rsc, 0x42L, text, PACKAGE="RSclient")
+RS.server.eval <- function(rsc, text) .Call(RS_ctrl_str, rsc, 0x42L, text)
 
-RS.server.source <- function(rsc, filename) .Call("RS_ctrl_str", rsc, 0x45L, filename, PACKAGE="RSclient")
+RS.server.source <- function(rsc, filename) .Call(RS_ctrl_str, rsc, 0x45L, filename)
 
-RS.server.shutdown <- function(rsc) .Call("RS_ctrl_str", rsc, 0x44L, "", PACKAGE="RSclient")
+RS.server.shutdown <- function(rsc) .Call(RS_ctrl_str, rsc, 0x44L, "")
 
-RS.switch <- function(rsc, protocol="TLS", verify=TRUE) .Call("RS_switch", rsc, protocol, verify, PACKAGE="RSclient")
+RS.switch <- function(rsc, protocol="TLS", verify=TRUE) .Call(RS_switch, rsc, protocol, verify)
 
-RS.authkey <- function(rsc, type="rsa-authkey") .Call("RS_authkey", rsc, type, PACKAGE="RSclient")
+RS.authkey <- function(rsc, type="rsa-authkey") .Call(RS_authkey, rsc, type)
 
 RS.assign <- function(rsc, name, value, wait = TRUE) {
   if (missing(value)) {
@@ -36,24 +36,24 @@ RS.assign <- function(rsc, name, value, wait = TRUE) {
     value <- name
     name <- sym.name
   }
-  .Call("RS_assign", rsc, serialize(list(name, value), NULL), wait, PACKAGE="RSclient")
+  .Call(RS_assign, rsc, serialize(list(name, value), NULL), wait)
 }
 
 RS.login <- function(rsc, user, password, pubkey, authkey) {
   if (missing(user) || missing(password)) stop("user and password must be specified")
-  .Call("RS_secauth", rsc, paste(c(user, password, ''), collapse="\n"), authkey, PACKAGE="RSclient")
+  .Call(RS_secauth, rsc, paste(c(user, password, ''), collapse="\n"), authkey)
 }
 
 RS.oobCallbacks <- function(rsc, send, msg) {
-  if (missing(send) && missing(msg)) return(.Call("RS_oob_cb", rsc, NULL, NULL, TRUE))
+  if (missing(send) && missing(msg)) return(.Call(RS_oob_cb, rsc, NULL, NULL, TRUE))
   if (missing(send) || missing(msg)) {
-    l <- .Call("RS_oob_cb", rsc, NULL, NULL, TRUE, PACKAGE="RSclient")
+    l <- .Call(RS_oob_cb, rsc, NULL, NULL, TRUE)
     if (missing(send)) send <- l$send
     if (missing(msg))  msg <- l$msg
   }
-  invisible(.Call("RS_oob_cb", rsc, send, msg, FALSE, PACKAGE="RSclient"))  
+  invisible(.Call(RS_oob_cb, rsc, send, msg, FALSE))  
 }
 
-print.RserveConnection <- function(x, ...) invisible(.Call("RS_print", x))
-`==.RserveConnection` <- function(e1, e2) .Call("RS_eq", e1, e2)
-`!=.RserveConnection` <- function(e1, e2) !.Call("RS_eq", e1, e2)
+print.RserveConnection <- function(x, ...) invisible(.Call(RS_print, x))
+`==.RserveConnection` <- function(e1, e2) .Call(RS_eq, e1, e2)
+`!=.RserveConnection` <- function(e1, e2) !.Call(RS_eq, e1, e2)
