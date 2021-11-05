502f827a3955726b258f82e2142dd117648e1dbc
Simon Urbanek <inbox@urbanek.info>
2021-11-05 17:55:04 +1300

    minor QAP fixes

--- src/qap_decode.c
+++ src/qap_decode.c
@@ -127,7 +127,7 @@ SEXP decode_to_SEXP(unsigned int **buf)
 	l = ln / 16;
 	val = allocVector(CPLXSXP, l);
 #ifdef NATIVE_COPY
-	memcpy(COMPLEX(val), b, sizeof(*COMPLEX(val)) * l);
+	memcpy(COMPLEX(val), b, sizeof(Rcomplex) * l);
 	b += l * 4;
 #else
 	i = 0;
@@ -185,24 +185,25 @@ SEXP decode_to_SEXP(unsigned int **buf)
 	{
 	    unsigned char *ie = (unsigned char*) b + ln;
 	    rlen_t n = 0;
-	    SEXP lh = R_NilValue;
-	    SEXP vr = allocVector(VECSXP, 1);
+	    SEXP vr = PROTECT(CONS(R_NilValue, R_NilValue));
+	    SEXP tail = vr;
 	    *buf = b;
-	    PROTECT(vr);
 	    while ((unsigned char*)*buf < ie) {
 		SEXP v = decode_to_SEXP(buf);
-		lh = CONS(v, lh);
-		SET_VECTOR_ELT(vr, 0, lh); /* this is our way of staying protected .. maybe not optimal .. */
+		/* CONS() protects its arguments if GC is needed so v is safe */
+		SEXP ne = CONS(v, R_NilValue);
+		SETCDR(tail, ne);
+		tail = ne;
 		n++;
 	    }
 #ifdef RSERV_DEBUG
-	    printf(" vector (%s), %d elements\n", (ty == XT_VECTOR) ? "generic" : ((ty == XT_VECTOR_EXP) ? "expression" : "string"), n);
+	    printf(" vector (%s), %ld elements\n", (ty == XT_VECTOR) ? "generic" : ((ty == XT_VECTOR_EXP) ? "expression" : "string"), (long) n);
 #endif
 	    val = PROTECT(allocVector((ty==XT_VECTOR) ? VECSXP : ((ty == XT_VECTOR_EXP) ? EXPRSXP : STRSXP), n));
-	    while (n > 0) {
-		n--;
-		SET_VECTOR_ELT(val, n, CAR(lh));
-		lh = CDR(lh);
+	    n = 0;
+	    while (CDR(vr) != R_NilValue) {
+		vr = CDR(vr);
+		SET_VECTOR_ELT(val, n++, CAR(vr));
 	    }
 #ifdef RSERV_DEBUG
 	    printf(" end of vector %lx/%lx\n",
